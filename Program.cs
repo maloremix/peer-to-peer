a8b9c69fa34cef75ed9839ebbc2d733675509a00
maloremix <maloremix@i.aparin@mail.ru>
2023-04-07 12:22:04 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -28,6 +28,7 @@ class Program
             while (true)
             {
                 TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine(client);
 
                 // Добавляем клиента в список клиентов
                 Console.WriteLine($"Подключен новый клиент: {client.Client.RemoteEndPoint}");
@@ -40,6 +41,7 @@ class Program
                     byte[] buffer = new byte[client.ReceiveBufferSize];
                     int bytesRead = stream.Read(buffer, 0, client.ReceiveBufferSize);
                     string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine(message);
                     Regex regex = new Regex(@"Клиент подключился с портом (\d+)");
                     Match match = regex.Match(message);
                     if (match.Success)
@@ -48,19 +50,15 @@ class Program
                         try
                         {
                             TcpClient newClient = new TcpClient();
-                            newClient.Connect("localhost", newClientPort); // Устанавливаем соединение с хостом и портом
-                            clients.Add(newClient); // Добавляем клиента в список
+                            client.Connect("localhost", newClientPort); // Устанавливаем соединение с хостом и портом
+                            clients.Add(client); // Добавляем клиента в список
                         }
                         catch (Exception e)
                         {
                             // Обработка ошибок
-                            Console.WriteLine($"Ошибка подключения к порту {newClientPort}: {e.Message}");
                         }
                     }
-                    else
-                    {
-                        Console.WriteLine(message);
-                    }
+                    Console.WriteLine(message);
 
                     client.Close();
                 });
@@ -83,12 +81,10 @@ class Program
                 NetworkStream stream = client.GetStream();
                 byte[] messageBytes = Encoding.ASCII.GetBytes("Клиент подключился с портом " + port);
                 stream.Write(messageBytes, 0, messageBytes.Length);
-                clients.Add(client);
             }
             catch (Exception e)
             {
                 // Обработка ошибок
-                Console.WriteLine($"Ошибка подключения к порту {remotePort}: {e.Message}");
             }
         }
 
@@ -98,16 +94,12 @@ class Program
             // Отправляем сообщение всем доступным клиентам
             foreach (var client in clients)
             {
+                Console.WriteLine(client);
                 try
                 {
                     NetworkStream stream = client.GetStream();
                     byte[] messageBytes = Encoding.ASCII.GetBytes(line);
                     stream.Write(messageBytes, 0, messageBytes.Length);
-                    // Читаем ответ от клиента
-                    byte[] buffer = new byte[client.ReceiveBufferSize];
-                    int bytesRead = stream.Read(buffer, 0, client.ReceiveBufferSize);
-                    string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                    Console.WriteLine(response);
                 }
                 catch (Exception e)
                 {
@@ -115,6 +107,7 @@ class Program
                 }
             }
         }
+
+        listenerThread.Abort();
     }
 }
-                
